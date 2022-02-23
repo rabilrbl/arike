65ee941ee0a33d0e8807d558cbc8fb5b4e5a3ee6
Mohammed Rabil <rabil@techie.com>
2022-02-23 17:03:20 +0530

    Disable Workflows

--- arike/apps/DistrictAdmin/views.py
+++ arike/apps/DistrictAdmin/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# Models
+from arike.apps.Facility.models import Facility
 
-# Create your views here.
+# import GenericViews
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.forms import ModelForm
+from django.urls import reverse_lazy
+User = 
+
+
+class UserIndexView(ListView):
+    """
+    District Admin User list page
+    """
+    template_name = 'DistrictAdmin/index.html'
+    context_object_name = "users"
+
+    def get_queryset(self):
+        return User.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Users'
+        return context
